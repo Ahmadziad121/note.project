a6c561139dc528ea6e18d2bbef7a3a0f8fc1f5d6
Ahmadziad121 <ahmadziad758@gmail.com>
2024-02-25 15:41:10 +0300

    3

--- note.project/src/note.js
+++ note.project/src/note.js
@@ -6,12 +6,14 @@ const NoteForm = ({ addNote }) => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [reminder, setReminder] = useState('');
-  
+  const max_title_length=30;
+  const max_body_length=200;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!title || !body) return;
-    addNote({ title, body,createdAt:new Date().toISOString(),reminder, });
+    addNote({ title :title.substring(0,max_title_length), body :body.substring(0,max_body_length),
+      createdAt:new Date().toISOString(),reminder, });
     setTitle('');
     setBody('');
     setReminder('')
@@ -19,9 +21,12 @@ const NoteForm = ({ addNote }) => {
 
   return (<div className='form'>
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
-      <textarea placeholder="Write your daily note" value={body} onChange={(e) => setBody(e.target.value)} ></textarea>
-      <input type="text" placeholder="Reminder" value={reminder} onChange={(e) => setReminder(e.target.value)}/>
+      <input type="text" placeholder="Title" value={title} required onChange={(e) => setTitle(e.target.value)}
+        maxLength={max_title_length}
+      /><span id='title'>{title.length}/{max_title_length}</span>
+      <textarea placeholder="Write your daily note" value={body} required onChange={(e) => setBody(e.target.value)}  maxLength={max_body_length} ></textarea>
+      <span id='body'>{body.length}/{max_body_length}</span>
+      <input type="text" placeholder="Reminder" value={reminder} required onChange={(e) => setReminder(e.target.value)}/>
 
       <button className='sub' type="submit">Add Note</button>
     </form></div>
